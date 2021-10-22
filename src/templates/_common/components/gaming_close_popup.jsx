e3f2d4125eb2ec257593f77394bd12b735f4e3e9
shuvohabib <shuvohabib@gmail.com>
2021-10-22 20:11:00 +0600

    update content

--- src/templates/_common/components/gaming_close_popup.jsx
+++ src/templates/_common/components/gaming_close_popup.jsx
@@ -6,22 +6,24 @@ const GamingClosePopup = () => (
             <div className='pop-up-header'>
                 <h3>Your Gaming account is scheduled to be closed</h3>
                 <p className='subtitle'>
-                As part of the changes in our product line-up,
-                we will be closing Gaming accounts belonging to UK clients.
+                As part of the changes in our product line-up, we will
+                be closing Gaming accounts belonging to our UK/Isle of Man clients.
                 </p>
                 <div className='deriv_banner_close' />
             </div>
             <div className='popup-content-body'>
                 <strong>What this means for you</strong>
                 <p>
-                    You can no longer trade digital options on DTrader, SmartTrader, DBot, and Binary Bot.
-                    You also can’t deposit funds into your Gaming account.
+                You can no longer trade digital options on any of our platforms.
+                You also can’t deposit funds into your Gaming account.
                 </p>
                 <p>
-                    Any open positions on digital options have been closed and will be refunded.
+                Any open positions on digital options have been closed with full payout.
                 </p>
                 <strong>What you need to do now</strong>
-                <p>Please proceed to withdraw all your funds from your Gaming account.</p>
+                <p>Please proceed to withdraw all your funds from your Gaming account 
+                    before <strong>30 November 2021</strong>.
+                </p>
             </div>
             <footer>
                 <button id='accept-btn' className='accept-btn'>OK, I understand</button>
