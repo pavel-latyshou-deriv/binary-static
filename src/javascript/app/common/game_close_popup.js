d0cf32274ed02f710b0543910196c06f086b0cd7
“pavel-latyshou-deriv” <pavel.latyshou@regentmarkets.com>
2021-10-22 16:55:59 +0300

    fix bug with button

--- src/javascript/app/common/game_close_popup.js
+++ src/javascript/app/common/game_close_popup.js
@@ -1,11 +1,12 @@
 const getElementById = require('../../_common/common_functions').getElementById;
 
+
 const ClosePopup = (() => {
     let el_gaming_popup, el_accept_btn;
 
     const onLoad = () => {
         el_gaming_popup = getElementById('gaming-close-popup');
-        el_accept_btn.getElementById('accept-btn');
+        el_accept_btn = getElementById('accept-btn');
         el_accept_btn.addEventListener('click', onClosePopup);
     };
 
