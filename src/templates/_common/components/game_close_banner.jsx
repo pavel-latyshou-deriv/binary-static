cef58ded7658ef8b9db9d34d79aa850893e5a220
shuvohabib <shuvohabib@gmail.com>
2021-10-22 16:50:41 +0600

    add popup

---
+++ src/templates/_common/components/game_close_banner.jsx
@@ -0,0 +1,34 @@
+import React, { useState }  from 'react';
+import PropTypes from 'prop-types';
+// eslint-disable-next-line import/no-unresolved
+import GamingClosePopup from './gaming_close_popup';
+
+const CloseBanner = ({ has_margin }) => {
+    
+    const [popup, setPopup] = useState(false);
+    return (
+        <div
+            id='close_banner_container'
+            className={`invisible ${has_margin ? 'has_margin' : ''}`}
+        >
+            <div className='close_banner_text'>
+                <img
+                    src={it.url_for('images/close-banner/icon_left.png')}
+                    className='close_banner_img'
+                />
+                <div className='close_banner_text_wrapper'>
+                    <h3>Your Gaming Acount is scheduled to be closed</h3>
+                    <p>Please proceed to withdraw your funds.</p>
+                </div>
+            </div>
+            <div onClick={() => setPopup(true)} className='close_banner_btn'>{it.L('Learn more')}</div>
+            {popup && <GamingClosePopup />}
+        </div>
+    );
+};
+
+CloseBanner.propTypes = {
+    has_margin: PropTypes.bool,
+};
+
+export default CloseBanner;
