9d82e397fb5ecc15dc55a41d5397ff4cfa2ce9c8
“pavel-latyshou-deriv” <pavel.latyshou@regentmarkets.com>
2021-10-22 12:56:15 +0300

    dirty version

--- src/templates/_common/components/game_close_banner.jsx
+++ src/templates/_common/components/game_close_banner.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 const CloseBanner = ({ has_margin }) => (
-    <div id='close_banner_container' className={`invisible ui-helper-clearfix ${has_margin ? 'has_margin' : '' }`}>
-        <img src={it.url_for(`images/close-banner/icon_left.png`)} className='close_banner_img' />
-        <div className='close_banner_text'>
+  <div
+    id="close_banner_container"
+    className={`invisible ${has_margin ? "has_margin" : ""}`}
+  >
+      <div className="close_banner_text">
+    <img
+      src={it.url_for(`images/close-banner/icon_left.png`)}
+      className="close_banner_img"
+    />
         <h3>Your Gaming Acount is scheduled to be closed</h3>
         <p>Please proceed to withdraw your funds.</p>
-        </div>
-        <div className='close_banner_btn'>{it.L('Try now')}</div>
-    </div>
+      </div>
+      <div className="close_banner_btn">{it.L("Learn more")}</div>
+  </div>
 );
 
 CloseBanner.propTypes = {
-    has_margin: PropTypes.bool,
+  has_margin: PropTypes.bool,
 };
 
 export default CloseBanner;
