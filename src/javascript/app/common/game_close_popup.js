82947131825b628506c8a84e82cf6f82a8794e12
shuvohabib <shuvohabib@gmail.com>
2021-10-24 21:26:57 +0600

    add fix getelementId

--- src/javascript/app/common/game_close_popup.js
+++ src/javascript/app/common/game_close_popup.js
@@ -4,8 +4,8 @@ const ClosePopup = (() => {
     let el_gaming_popup, el_accept_btn;
 
     const onLoad = () => {
-        el_gaming_popup.setVisibility(0);
         el_gaming_popup = getElementById('gaming-close-popup');
+        el_gaming_popup.setVisibility(0);
         el_accept_btn = getElementById('accept-btn');
         el_accept_btn.addEventListener('click', onClosePopup);
     };
