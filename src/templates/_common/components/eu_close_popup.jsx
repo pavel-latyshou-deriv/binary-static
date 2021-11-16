11a3aecf51a611b8c610057becb4011aa639767a
“pavel-latyshou-deriv” <pavel.latyshou@regentmarkets.com>
2021-11-16 16:38:38 +0300

    mobile title focus

--- src/templates/_common/components/eu_close_popup.jsx
+++ src/templates/_common/components/eu_close_popup.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const EuClosePopup = () => (
     <div id='eu-close-popup' className='invisible gaming-close-popup'>
         <div className='popup-content'>
-            <div className='pop-up-header'>
+            <div className='pop-up-header' onFocus={true}>
                 <h3>Your Options Account is scheduled to be closed</h3>
                 <p className='subtitle'>
                 As part of the changes in our product line-up,
